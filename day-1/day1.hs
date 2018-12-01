3048e34bfa37781870cf68f921004f431b36f888
Alessio Zakaria <alessio.b.zakaria@googlemail.com>
2018-12-01 20:27:36 +0000

    day 1 task 2

---
+++ day-1/day1.hs
@@ -0,0 +1,31 @@
+module Main where
+
+import Data.List.Split
+import qualified Data.IntSet as IntSet
+
+
+main :: IO ()
+main = do
+    handle <- readFile "./input.txt"
+    let x  = init $ splitOn "\n" handle
+    print $ test $  parseNum <$> x
+
+
+repList :: [a] -> [a]
+repList  = concat . repeat
+
+thing ((x:xs), total) = (xs,  total + x)
+
+genSumList xs = map snd <$> tail $ iterate thing ( xs, 0)
+
+findRepeat :: [Int] -> Int
+findRepeat (x : xs) = go x xs (IntSet.singleton 0)
+    where
+        go y as zs = if (IntSet.member) y zs
+                     then y
+                     else case as of
+                               [] -> error "the grinch"
+                               (b:bs) -> go b bs ((IntSet.insert) y zs)
+
+
+test = findRepeat . genSumList . repList
